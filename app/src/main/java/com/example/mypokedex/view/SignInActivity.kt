41894aaff0c82ade3642f4bac8dcbd98e8f9fadc
Pedro Miguel Pérez Torres <pmpedrotorres@gmail.com>
2023-11-14 07:08:03 -0600

    Added SignIn and SignUp activities designs

--- app/src/main/java/com/example/mypokedex/view/SignInActivity.kt
+++ app/src/main/java/com/example/mypokedex/view/SignInActivity.kt
@@ -3,17 +3,17 @@ package com.example.mypokedex.view
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mypokedex.databinding.ActivityMainBinding
+import com.example.mypokedex.databinding.ActivitySignInBinding
 import com.example.mypokedex.viewModel.PokemonViewModel
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
 
     private val viewModel: PokemonViewModel by viewModels()
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivitySignInBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
     }
